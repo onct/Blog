49752905a095b33d6212833978687bfc9dbd6e33
oncat <2251883836@qq.com>
2020-11-26 17:30:07 +0800

    Update App.vue

--- myblog/src/App.vue
+++ myblog/src/App.vue
@@ -1,7 +1,7 @@
 <style scoped>
 .layout {
   border: 1px solid #d7dde4;
-  background: #f5f7f9;
+  background: #fff;
   position: relative;
   border-radius: 4px;
   overflow: hidden;
@@ -29,34 +29,15 @@
       </Header>
       <Layout>
         <Layout :style="{height:'93vh',padding: '0 24px 24px'}">
-
-          <Content :style="{padding: '24px', background: '#fff'}">Content</Content>
+          <Content :style="{padding: '24px', background: '#fff',width:'500px'}">Content</Content>
         </Layout>
-        <Sider hide-trigger :style="{background: '#fff'}">
-          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>Item 1
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template slot="title">
-                <Icon type="ios-keypad"></Icon>Item 2
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template slot="title">
-                <Icon type="ios-analytics"></Icon>Item 3
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
-          </Menu>
+        <Sider hide-trigger :style="{background: '#f5f7f9'}">
+           <Card :style="{position: 'fixed',margin:'50px 0'}">
+                <p slot="title">The standard card</p>
+                <p>Content of card</p>
+                <p>Content of card</p>
+                <p>Content of card</p>
+            </Card>
         </Sider>
       </Layout>
     </Layout>
